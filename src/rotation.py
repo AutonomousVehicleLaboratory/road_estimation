31698554fceb0bf68b82bd4f80a7edd63dc56077
ZHYHenryZhang <henryzhang19951213@gmail.com>
2020-02-14 12:01:21 -0800

    added quaternion and axis angle transformation

--- src/rotation.py
+++ src/rotation.py
@@ -1,4 +1,4 @@
-""" rotation operation
+""" rotation
 
 Author: Henry Zhang
 Date:January 27, 2020
@@ -11,6 +11,68 @@ import numpy as np
 
 
 # classes
+
+class Quaternion:
+    def __init__(self, x=0, y=0, z=0, w=1.0):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+        self.w = float(w)
+        self.normalize()
+    
+    @classmethod
+    def create_quaternion_from_vector_to_vector(cls, v1, v2):
+        """ from two direction vectors, create the quaternion 
+        formulas from https://stackoverflow.com/questions/1171849/finding-quaternion-representing-the-rotation-from-one-vector-to-another
+        verified by create axis_angle then convert to quaternion
+        Params:
+            v1, v2: direction vectors, expected 3 by 1
+        Return:
+            q: Quaternion class
+        """
+        q = cls()
+        direction = np.cross(v1.reshape([-1]), v2.reshape([-1]))
+        q.x = direction[0]
+        q.y = direction[1]
+        q.z = direction[2]
+        q.w = np.sqrt((np.linalg.norm(v1) ** 2) * (np.linalg.norm(v2) ** 2)) + np.matmul(v1.T, v2).item()
+        q.normalize()
+        return q
+    
+    def normalize(self):
+        norm = np.sqrt(np.sum(np.array([self.x, self.y, self.z, self.w])**2))
+        self.x /= norm
+        self.y /= norm
+        self.z /= norm
+        self.w /= norm
+    
+    def to_list_xyzw(self):
+        orientation = [self.x, self.y, self.z, self.w]
+        return orientation
+
+class AxisAngle:
+    def __init__(self, w):
+        self.w = w
+        self.angle = np.linalg.norm(w)
+        self.axis = np.array(w) / self.angle
+    
+    @classmethod
+    def create_axisangle_from_vector_to_vector(cls, v1, v2):
+        direction = np.cross(v1.reshape([-1]), v2.reshape([-1]))
+        axis = direction / np.linalg.norm(direction)
+        angle = np.arccos( np.matmul(v1.reshape((3,1)).T, v2.reshape((3,1))).item() / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+        w = axis * angle
+        return cls(w)
+    
+    def to_quaternion(self):
+        w = np.cos(self.angle / 2.0)
+        direction = np.sin(self.angle / 2.0) * self.axis
+        x = direction[0]
+        y = direction[1]
+        z = direction[2]
+        return Quaternion(x, y, z, w)
+        
+
 class Rotation:
     def __init__(self, roll=0.0, pitch=0.0, yaw=0.0):
         self.roll = roll
@@ -27,10 +89,20 @@ class Rotation:
         m_yaw = np.array([[np.cos(self.yaw), -np.sin(self.yaw), 0],
                            [np.sin(self.yaw), np.cos(self.yaw), 0],
                            [0, 0, 1]])
-        m_euler = m_yaw @ m_pitch @ m_roll
+        m_euler = np.matmul( np.matmul(m_yaw, m_pitch),  m_roll)
         return m_euler
+    
+    
 
 # functions
+def test_vector_to_vector():
+    v1 = np.array([[0, 0, 1]]).T
+    v2 = np.array([[-0.5, 0, 1.732/2.0]]).T
+    q = Quaternion.create_quaternion_from_vector_to_vector(v1, v2)
+    rot = AxisAngle.create_axisangle_from_vector_to_vector(v1, v2)
+    q2 = rot.to_quaternion()
+    pass
+
 def test_get_matrix():
     rot = Rotation(roll=9.0/180*np.pi)
     mat = rot.getMatrix()
@@ -38,6 +110,7 @@ def test_get_matrix():
 
 # main
 def main():
+    test_vector_to_vector()
     test_get_matrix()
 
 if __name__ == "__main__":
