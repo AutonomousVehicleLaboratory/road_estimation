38568324f8574b6b13bb2ac45ade83435d781460
Henry Zhang <hyzhang@eng.ucsd.edu>
2020-03-09 10:33:54 -0700

    publish inliers and outliers

--- src/road_estimation.py
+++ src/road_estimation.py
@@ -35,6 +35,7 @@ from ransac import RANSAC
 from camera import camera_setup_1, camera_setup_6
 from bounding_box import BoundingBox
 from utils import clip_pcd_by_distance_plane
+from utils_ros import create_point_cloud
 from vis import visualize_marker
 
 np.set_printoptions(precision=3)
@@ -45,6 +46,8 @@ plane = Plane3D(-0.157, 0, 0.988, 1.9)
 global pub_plane
 global pub_intersect_markers
 global pub_plane_markers
+global pub_pcd_inlier
+global pub_pcd_outlier
 # classes
 
 
@@ -107,6 +110,8 @@ def estimate_plane(pcd):
     pt1 = np.array([0,0,0])
     threshold = [6.0, -3.0]
     pcd_close, _ = clip_pcd_by_distance_plane(pcd, vec1, vec2, pt1, threshold)
+    seed=0
+    np.random.seed(seed)
     plane1, _, _, _ = sac.ransac(pcd_close.data.T, constraint=plane.param, constraint_threshold=0.5)
     # vis(plane1, pcd, dim_2d=True)
 
@@ -135,7 +140,10 @@ def estimate_plane(pcd):
             print(i, "{:.1f}".format(depth_thred_min), "{:.1f}".format(depth_thred_max), sum_inner, sum_outer, "{:.4f}".format(ratio))
     
     print('Plane params:', plane1.param.T)
-    return plane1
+    threshold_inlier = 0.15
+    pcd_inlier = pcd_close.data[:,distance <= threshold_inlier]
+    pcd_outlier = pcd_close.data[:,distance > threshold_inlier]
+    return plane1, pcd_inlier, pcd_outlier
 
 def create_and_publish_plane_markers(plane):
     v1 = np.array([[0, 0, 1.0]]).T
@@ -153,7 +161,7 @@ def create_and_publish_plane_markers(plane):
     return marker_array
 
 def pcd_callback(msg):
-    global plane, pub_plane_markers, pub_plane
+    global plane, pub_plane_markers, pub_plane, pub_pcd_inlier, pub_pcd_outlier
     rospy.logwarn("pcd height:%d, width:%d", msg.height, msg.width)
     pcd_original = np.empty((msg.width,3))
     for i, el in enumerate( pc2.read_points(msg, field_names = ("x", "y", "z"), skip_nans=True)):
@@ -162,17 +170,23 @@ def pcd_callback(msg):
     # print(type(pcd_original[0][0]))
     # print(len(pcd_original))
     pcd = PointCloud(pcd_original.T)
-    plane = estimate_plane(pcd)
+    plane, pcd_inlier, pcd_outlier = estimate_plane(pcd)
     marker_array = create_and_publish_plane_markers(plane)
     plane_msg = Plane()
     plane_msg.coef[0], plane_msg.coef[1], plane_msg.coef[2], plane_msg.coef[3] = plane.a, plane.b, plane.c, plane.d
     pub_plane_markers.publish(marker_array)
     pub_plane.publish(plane_msg)
+
+    pcd_msg_inlier = create_point_cloud(pcd_inlier.T, frame_id='velodyne')
+    pcd_msg_outlier = create_point_cloud(pcd_outlier.T, frame_id='velodyne')
+    pub_pcd_inlier.publish(pcd_msg_inlier)
+    pub_pcd_outlier.publish(pcd_msg_outlier)
+
     rospy.logwarn("Finished plane estimation")
 
 # main
 def main():
-    global pub_intersect_markers, pub_plane_markers, pub_plane
+    global pub_intersect_markers, pub_plane_markers, pub_plane, pub_pcd_inlier, pub_pcd_outlier
     rospy.init_node("road_estimation")
     
     fig = plt.figure(figsize=(16,8))
@@ -192,6 +206,8 @@ def main():
     pub_intersect_markers = rospy.Publisher("/vision_objects_position_rviz", MarkerArray, queue_size=10)
     pub_plane_markers = rospy.Publisher("/estimated_plane_rviz", MarkerArray, queue_size=10)
     pub_plane = rospy.Publisher("/estimated_plane", Plane, queue_size = 10)
+    pub_pcd_inlier = rospy.Publisher("/points_inlier", PointCloud2, queue_size=10)
+    pub_pcd_outlier = rospy.Publisher("/points_outlier", PointCloud2, queue_size=10)
 
     rospy.spin()
     # plt.show(block=True)
